a523030e91b7fc09df8d1cb8b20896c008c61907
Pete Croke <pete@orbitmedia.com>
2015-07-23 00:22:55 -0500

    Pete: add bootstrap image class in template

--- themes/pete_subtheme/template.php
+++ themes/pete_subtheme/template.php
@@ -4,3 +4,13 @@
  * @file
  * template.php
  */
+
+/**
+ *
+ * add bootstrap image class to images
+ */
+
+function pete_subtheme_preprocess_image_style(&$vars) {
+  $vars['attributes']['class'][] = 'img-thumbnail';
+  // can be 'img-rounded', 'img-circle', or 'img-thumbnail'
+}
